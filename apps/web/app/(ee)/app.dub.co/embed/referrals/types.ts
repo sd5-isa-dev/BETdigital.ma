f8a6a6149a0903ce84b10170979db2d1bd7f60fa
sd5-isa-dev <saddenmohamad96@gmail.com>
2026-06-20 17:12:58 +0000

    fix: remove dead referral/appsflyer/hubspot routes referencing deleted cron/partner code

--- apps/web/app/(ee)/app.dub.co/embed/referrals/types.ts
+++
@@ -1,4 +0,0 @@
-import { ReferralsEmbedLinkSchema } from "@/lib/zod/schemas/referrals-embed";
-import * as z from "zod/v4";
-
-export type ReferralsEmbedLink = z.infer<typeof ReferralsEmbedLinkSchema>;
